e69e7c4d75d31f9b867a4b1fd1a7aa5ca35a31c8
Cuma Bozkurt <myderdom72@gmail.com>
2023-01-18 22:51:29 +0300

    Html css ornekleri..

---
+++ ornek8.js
@@ -0,0 +1,20 @@
+function con1() {
+  spn1.classList.add("yek");
+  console.log(spn1);
+}
+function con2() {
+  spn2.classList.add("do");
+  console.log(spn2.classList.item(1));
+}
+function con3() {
+  spn3.classList.add("se");
+  console.log(spn3.classList.item(1));
+}
+function con4() {
+  spn4.classList.add("car");
+  console.log(spn4.classList.item(1));
+}
+function con5() {
+  spn5.classList.add("penc");
+  console.log(spn5.classList.item(1));
+}
